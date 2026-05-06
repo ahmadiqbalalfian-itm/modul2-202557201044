9cedb3826d7e7dc5f88aa410f372b1aefb62ff97
ahmadiqbalalfian-itm <ahmadiqbalalfian@gmail.com>
2026-05-06 10:14:33 +0700

    komen dikit

--- src/percobaan3methodjop/percobaan3methodjop.java
+++ src/percobaan3methodjop/percobaan3methodjop.java
@@ -8,15 +8,15 @@ public class percobaan3methodjop {
     
     //kita awali dengan method operasi dulu
     //persegi panjang dulu
-    double lpp(double p, double l){
-        return (p*l);
+    double lpp(double p, double l){//pakai variabel p dan l dari method main
+        return (p*l); // rumus pesregi panjang
     }
         //segitiga
-    double lsegitiga(double a, double t){
-        return (0.5*a*t);
+    double lsegitiga(double a, double t){//menggunakan variabel a dan t dari method main
+        return (a*t*0.5); //rumus segitiga
     }
     //lingkaran
-    double lsegitiga(double r){
-        return (Math.PI*Math.pow(r, 2));
+    double lsegitiga(double r){//meminjam r dari method main
+        return (Math.PI*Math.pow(r, 2));//rumus luas lingkaran
     }
 }
