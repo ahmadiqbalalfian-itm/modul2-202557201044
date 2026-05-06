4214b2a1c127e2a7eb1ebf11bf79bfbf233c0218
ahmadiqbalalfian-itm <ahmadiqbalalfian@gmail.com>
2026-05-06 07:52:25 +0700

    judul dan scanner

--- src/percobaan2method/percoban2method.java
+++ src/percobaan2method/percoban2method.java
@@ -4,6 +4,8 @@ public class percoban2method {
 
     public static void main(String[] args) {
         percoban2method alatiqbal = new percoban2method();
-        
+        System.out.println("MENGHITUNG VOLUME BANGUN RUANG BERBASIS LINGKARAN\n1. Tabung\n2. Kerucut\n3. Bola");
+        Scanner firamasuk = new Scanner(System.in);
+
     }
 }
