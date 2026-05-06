94e7ed0021e8d69efd2ef39db98edbb2f05d6ae7
ahmadiqbalalfian-itm <ahmadiqbalalfian@gmail.com>
2026-05-06 08:28:56 +0700

    rapikan method

--- src/percobaan2method/percoban2method.java
+++ src/percobaan2method/percoban2method.java
@@ -1,20 +1,6 @@
 package percobaan2method;
 import java.util.Scanner;
 public class percoban2method {
-
-    public static void main(String[] args) {
-        percoban2method alatiqbal = new percoban2method();
-        System.out.println("MENGHITUNG VOLUME BANGUN RUANG BERBASIS LINGKARAN\n1. Tabung\n2. Kerucut\n3. Bola");
-        
-        Scanner firamasuk = new Scanner(System.in);
-        
-        System.out.print("Jari-jari Lingkarannya Berapa?\t: ");
-        double r = firamasuk.nextDouble();
-        
-        System.out.print("Tinggi Bangun Ruangnya?\t : ");
-        double t = firamasuk.nextDouble();
-        
-    }
     
     //kita masukkan rumus-rumus untuk setiap bangun ruang
     //lingkaran lebih dulu karena basisnya
@@ -41,5 +27,17 @@ public class percoban2method {
     double v_bola (double r){
         double v = (4.0/3.0)/*rumus dari volume bola*/*Math.pow(r, 3)/*pangkat*/*Math.PI/*phi (lebih detail dari 3,14 dan 22/7)*/;
         return v;//simpan volem bola
+    }
+    
+    public static void main(String[] args) {//method main untuk program yang memerlukan output
+        percoban2method alatiqbal = new percoban2method();
+        /*judulnya*/System.out.println("MENGHITUNG VOLUME BANGUN RUANG BERBASIS LINGKARAN\n1. Tabung\n2. Kerucut\n3. Bola");
+        /*import scanner*/Scanner firamasuk = new Scanner(System.in);
+        //deklarasi inti, bahan utama dari bangun ruang basis lingkaran
+        System.out.print("Jari-jari Lingkarannya Berapa?\t: ");
+        double r = firamasuk.nextDouble();
+        System.out.print("Tinggi Bangun Ruangnya?\t : ");
+        double t = firamasuk.nextDouble();
+        
     }
 }
