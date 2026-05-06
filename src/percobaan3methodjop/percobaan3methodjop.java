4baa0bc88e98c0ad2d60a67581caed7a3bd08f60
ahmadiqbalalfian-itm <ahmadiqbalalfian@gmail.com>
2026-05-06 10:42:55 +0700

    buat jembatan method main ke method luar

--- src/percobaan3methodjop/percobaan3methodjop.java
+++ src/percobaan3methodjop/percobaan3methodjop.java
@@ -1,7 +1,7 @@
 package percobaan3methodjop;
 import javax.swing.JOptionPane;
 public class percobaan3methodjop {
-
+    
     //kita awali dengan method operasi dulu
     //persegi panjang dulu
     double lpp(double p, double l){//pakai variabel p dan l dari method main
@@ -18,19 +18,24 @@ public class percobaan3methodjop {
     
          //kita perlu deklarasikan variable yang akan digunakan method operasi
         //ini mending main taruh bawah biar gampang ngaturnya, jadi rumus dulu baru output gitu
-        public static void main(String[] args) {
-        
+    public static void main(String[] args) {
+        //untuk menyimpan nilai dari methodluar ke method main
+        percobaan3methodjop fira = new percobaan3methodjop();
+
         //operasi untuk luas persegi panjang
-        double p = Double.parseDouble(JOptionPane.showInputDialog(null,"Berapa Nilai Sisi Panjang?","Menghitung Luas Persegi Panjang",(3)));
-        double l = Double.parseDouble(JOptionPane.showInputDialog(null,"Berapa Nilai Sisi Lebar?","Menghitung Luas Persegi Panjang",(3)));
-        
+        double p = Double.parseDouble(JOptionPane.showInputDialog(null, "Berapa Nilai Sisi Panjang?", "Menghitung Luas Persegi Panjang", (3)));
+        double l = Double.parseDouble(JOptionPane.showInputDialog(null, "Berapa Nilai Sisi Lebar?", "Menghitung Luas Persegi Panjang", (3)));
+
         //operasi untuk luas segitiga
-        double a = Double.parseDouble(JOptionPane.showInputDialog(null,"Berapa Nilai Alas Segitiga?","Menghitung Luas Segitiga",(3)));
-        double t = Double.parseDouble(JOptionPane.showInputDialog(null,"Berapa Nilai Tinggi Alas Segitiga?","Menghitung Luas Segitiga",(3)));
-        
+        double a = Double.parseDouble(JOptionPane.showInputDialog(null, "Berapa Nilai Alas Segitiga?", "Menghitung Luas Segitiga", (3)));
+        double t = Double.parseDouble(JOptionPane.showInputDialog(null, "Berapa Nilai Tinggi Alas Segitiga?", "Menghitung Luas Segitiga", (3)));
+
         //operasi untuk luas lingkaran
-        double r = Double.parseDouble(JOptionPane.showInputDialog(null,"Berapa Nilai Jari-jari?","Menghitung Luas Lingkaran",(3)));
-    
+        double r = Double.parseDouble(JOptionPane.showInputDialog(null, "Berapa Nilai Jari-jari?", "Menghitung Luas Lingkaran", (3)));
+
+        //lanjut deklarasi varibel untuk menyimpan dan menampilkan hasil akhir
+        double persegipanjang = fira.lpp(p, l);
+        
         
     }
 }
