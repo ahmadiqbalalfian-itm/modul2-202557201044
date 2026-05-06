b270c1cf64db963718dfab53dafc4a0a76827582
ahmadiqbalalfian-itm <ahmadiqbalalfian@gmail.com>
2026-05-06 09:14:43 +0700

    push

--- src/percobaan2method/percoban2method.java
+++ src/percobaan2method/percoban2method.java
@@ -1,36 +1,29 @@
 package percobaan2method;
-
 import java.util.Scanner;
-
 public class percoban2method {
-
     //kita masukkan rumus-rumus untuk setiap bangun ruang
     //lingkaran lebih dulu karena basisnya
     double l_bulat(double r) {//impor variabel dari method main
         double l = Math.pow(r, 2)/*pangkat*/ * Math.PI/*phi*/;
         return l; //simpan luas untuk digunakan bangun ruang lain
     }
-
     //selanjutnya kerucut
     double vkerucut(double r, double t) {//ini mengimpor variabel yang diperlukan
         double l = l_bulat(r);//menghitung luas dari nilai yang disimpan method l_bulat
         double v = l * (t / 3.0);//rumusnya biar agak pendek. 3 harus dikasih koma karena double
         return v;//simpan volume keurucut
     }
-
     //kemudian tabung
     double vtabung(double r, double t) {//impor variabel jari-jari dan tinggi
         double l = l_bulat(r); //luas dari l_bulat
         double v = l * t;//rumusnya biar agak pendek. 3 harus dikasih koma karena double
         return v;//simpan volume keurucut
     }
-
     //lanjut bola
     double v_bola(double r) {
         double v = (4.0 / 3.0)/*rumus dari volume bola*/ * Math.pow(r, 3)/*pangkat*/ * Math.PI/*phi (lebih detail dari 3,14 dan 22/7)*/;
         return v;//simpan volem bola
     }
-
     public static void main(String[] args) {//method main untuk program yang memerlukan output
         percoban2method iqbal = new percoban2method();
         /*judulnya*/
@@ -54,7 +47,6 @@ public class percoban2method {
         System.out.println("VOLUME BOLA \t\t= " + bola);
         System.out.println("VOLUME TABUNG \t\t= " + tabung);
         System.out.println("VOLUME KERUCUT \t\t= " + kerucut + "\n\n");
-
         System.out.println("\t>>>>>>>>BONUS<<<<<<<<<\n\nLUAS LIINGKARAN/BULAT\t= " + bulat);
     }
 }
