e4233dafafd1e824c5065ad349144d4cad63d742
ahmadiqbalalfian-itm <ahmadiqbalalfian@gmail.com>
2026-05-06 10:05:34 +0700

    deklarasi variabel inti

--- src/percobaan3methodjop/percobaan3methodjop.java
+++ src/percobaan3methodjop/percobaan3methodjop.java
@@ -1,5 +1,10 @@
 package percobaan3methodjop;
 public class percobaan3methodjop {
+    //kita perlu deklarasikan variable yang akan digunakan method operasi
+    public static void main(String[] args) {
+        //ini deklarasi untuk variabel utama yang akan digunakan pada method operasi
+        double p,l,a,t,r;
+    }
     //kita awali dengan method operasi dulu
     //persegi panjang dulu
     //segitiga
