731890142ea04f0170358dbeaf493bc21bdc910a
ahmadiqbalalfian-itm <ahmadiqbalalfian@gmail.com>
2026-05-06 07:54:44 +0700

    input diameter (d)

--- src/percobaan2method/percoban2method.java
+++ src/percobaan2method/percoban2method.java
@@ -5,7 +5,12 @@ public class percoban2method {
     public static void main(String[] args) {
         percoban2method alatiqbal = new percoban2method();
         System.out.println("MENGHITUNG VOLUME BANGUN RUANG BERBASIS LINGKARAN\n1. Tabung\n2. Kerucut\n3. Bola");
+        
         Scanner firamasuk = new Scanner(System.in);
-
+        
+        System.out.print("Diameter Lingkarannya Berapa?\t: ");
+        double d = firamasuk.nextDouble();
+        
+        
     }
 }
